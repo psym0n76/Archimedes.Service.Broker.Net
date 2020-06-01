30c7ea0a695d1ca96f06e57e51ac051648423b8b
Simon Burton <burt76@outlook.com>
2020-06-01 21:43:32 +0100

    testing queue

--- Fx.Broker.Fxcm.Runner/Program.cs
+++ Fx.Broker.Fxcm.Runner/Program.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.Configuration;
+using Archimedes.Library.Message;
+using Archimedes.Library.Message.Dto;
+using EasyNetQ;
 using NLog;
 
 namespace Fx.Broker.Fxcm.Runner
@@ -7,6 +11,7 @@ namespace Fx.Broker.Fxcm.Runner
     public class Program
     {
         private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
         public static void Main(string[] args)
         {
             try
@@ -52,6 +57,36 @@ namespace Fx.Broker.Fxcm.Runner
                 _logger.Error(e, "Stopped program because of exception");
             }
 
+        }
+
+        private void TestQueue()
+        {
+            _logger.Info("Test Queue");
+
+            const string Host = "host=localhost";
+
+            var price = new ResponsePrice()
+            {
+                Status = "Test",
+                Payload = new List<PriceDto>(){new PriceDto(){AskClose = 1.2}},
+                Text = "This is working"
+            };
+
+            _logger.Info(price);
+
+            try
+            {
+                using (var bus = RabbitHutch.CreateBus(Host))
+                {
+                    bus.Publish(price);
+                    _logger.Info("Posted PriceResponse messages. Hit <return> to quit");
+                }
+            }
+            catch (Exception e)
+            {
+
+                _logger.Error(e,"Failed to post to Queue");
+            }
         }
     }
 }
