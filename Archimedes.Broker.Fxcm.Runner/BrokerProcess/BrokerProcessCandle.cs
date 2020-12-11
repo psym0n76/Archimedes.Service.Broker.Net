caaff63e574a4d8e0c344913005ead1d97385434
Simon Burton <burt76@outlook.com>
2020-12-11 11:48:07 +0000

    fixed logging text

--- Archimedes.Broker.Fxcm.Runner/BrokerProcess/BrokerProcessCandle.cs
+++ Archimedes.Broker.Fxcm.Runner/BrokerProcess/BrokerProcessCandle.cs
@@ -91,7 +91,7 @@ namespace Archimedes.Broker.Fxcm.Runner
                 request.StartDate, request.EndDate);
 
             _batchLog.Update(_logId,
-                $"FXCM Candles returned on Market: {request.Market} Granularity: {request.Interval}{request.TimeFrame} Count: {candles.Count}");
+                $"FXCM Candle Response: {request.Market} TimeFrame: {request.Interval}{request.TimeFrame} Records: {candles.Count}");
 
             BuildResponse(request, candles);
         }
@@ -112,7 +112,7 @@ namespace Archimedes.Broker.Fxcm.Runner
             var offer = offers.FirstOrDefault(o => o.Currency == request.Market);
 
             _batchLog.Update(_logId,
-                $"FXCM Candle Request {nameof(request.Market)}: {request.Market}  {nameof(request.TimeFrame)}: {request.Interval}{request.TimeFrame} {nameof(request.StartDate)}: {request.StartDate} {nameof(request.EndDate)}: {request.EndDate}\n");
+                $"FXCM Candle Request: {request.Market} TimeFrame: {request.Interval}{request.TimeFrame} {nameof(request.StartDate)}: {request.StartDate} {nameof(request.EndDate)}: {request.EndDate}\n");
 
             return offer.OfferId;
         }
